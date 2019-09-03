94e2a46c9b3c1076ce1a381b8769aeeb0dedbc39
gassoumiraouf <53814095+gassoumiraouf@users.noreply.github.com>
2019-09-03 15:39:48 +0200

    Add files via upload

---
+++ list.js
@@ -0,0 +1,49 @@
+function newActivity(){
+    var li=document.createElement('li')
+    var myInput=document.getElementById('the-input').value
+    var x=document.createTextNode(myInput)
+    li.appendChild(x)
+    if (myInput==='')
+    alert ('ekteb valeur se3a')
+    else {
+    
+    document.getElementById("myUl").appendChild(li);}
+    document.getElementById("the-input").value = ""
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    
+      
+        for (i = 0; i < close.length; i++) {
+          close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+          }
+        }
+
+}
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
